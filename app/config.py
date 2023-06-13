9ae828fe4b44a97bab5aab5f5fc4149c469c5f0d
Jhon Dany Mamani <jdany.mc@gmail.com>
2023-06-12 20:24:52 -0500

    second commit

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456.a@localhost/db_shop_g20'
+    #SQLALCHEMY_DATABASE_URI = 'mysql://root:123456.a@localhost/db_shop_g20'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://db_shop_g20_user:pE8WvuG1h4fJEmLvISyAbXADVLmAN7JD@dpg-ci3s37liuie031lrs1b0-a.ohio-postgres.render.com/db_shop_g20'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'secret'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
